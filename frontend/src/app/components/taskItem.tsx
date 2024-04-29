ddeaf7785e39310b0c41984d1eb46e17f67e874f
Silas Supke <sisuit00@hs-esslingen.de>
2024-04-29 10:18:20 +0000

    Änderung Component taskItem

--- frontend/src/app/components/taskItem.tsx
+++ frontend/src/app/components/taskItem.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 
-export default function TaskItem() {
-  return (
+interface TaskItemProps {
+    task: string;
+    taskArray: [];
+    onDelete: () => void; // Methode zum Löschen der Aufgabe
+}
+
+export default function TaskItem({ task, taskArray, onDelete }: TaskItemProps) {
+    return (
     <div className="flex justify-between items-center gap-x-3">
           <p className="flex-1 border border-gray-300 m-2 p-2 px-4 peer/add transition-all duration-300">
-            TASK 1
+            {task}
           </p>
-          <button className="bg-slate-800 text-white p-2 px-4 rounded-full w-max block peer-placeholder-shown/add:w-0">Delete Task</button>
+          <button onClick={onDelete} className="bg-slate-800 text-white p-2 px-4 rounded-full w-max block peer-placeholder-shown/add:w-0">Delete Task</button>
         </div>
     );
 }
